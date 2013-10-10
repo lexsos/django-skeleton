ed4cf791c997268c0390d9ba3fcbe3040284fa15
Sosnin Alexander <sosnin@trilan.ru>
2013-10-10 08:36:28 +0400

    Add dev/prod configs

---
+++ project/settings/development.py
@@ -0,0 +1,16 @@
+import os
+
+from .common import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'sqlite3.db'),
+    }
+}
+
+
---
+++ project/settings/production.py
@@ -0,0 +1,14 @@
+from .common import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+    }
+}
