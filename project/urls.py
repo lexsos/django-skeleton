e18c960803922949496078bf11dc1a98a06ddba2
Sosnin Alexander <sosnin@trilan.ru>
2013-10-11 08:19:11 +0400

    Fix media

--- project/urls.py
+++ project/urls.py
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^testapp/', include('testapp.urls')),
-)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
