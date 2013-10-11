4310d1cef56839231434b6b4e13c93292b13ce0e
Sosnin Alexander <sosnin@trilan.ru>
2013-10-11 12:38:56 +0400

    Fix TEMPLATE_CONTEXT_PROCESSORS

--- project/settings/common.py
+++ project/settings/common.py
@@ -82,8 +82,14 @@ TEMPLATE_DIRS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
 )
 
 PROJECT_APPS = (
@@ -141,10 +147,3 @@ LOGGING = {
 LOCALE_PATHS = (
     os.path.join(PROJECT_ROOT, 'locale'),
 )
-
-
-THUMBNAIL_ALIASES = {
-    '': {
-        'avatar': {'size': (50, 50), 'crop': True},
-    },
-}
