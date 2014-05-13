e8de61ac5e96335a8a832e206b04b0903ec28dc6
lexsos <ale-sosnin@yandex.ru>
2014-05-13 13:25:47 +0400

    Add gunicorn to conf

--- project/settings/production.py
+++ project/settings/production.py
@@ -9,6 +9,21 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'project',
+        'NAME': '',
+        'USER': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'PASSWORD': '',
     }
 }
+
+INSTALLED_APPS += (
+    'gunicorn',
+)
+
+USE_X_FORWARDED_HOST = True
+
+try:
+    from .secret import SECRET_KEY
+except:
+    print 'Need secret.py file with SECRET_KEY'
